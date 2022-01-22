0646c7fbb7118dd389abb57deec6d97f1fbaf27d
TeszPaulina <paulinajelinskaa@gmail.com>
2022-01-22 18:49:02 +0100

    Przykład 4

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
         /* przykład 4. Blok try catch */
-
         /* Przykład ArithmeticException */
         /* w bloku try spradzamy poporawność działania arytmetycznego */
         try {
